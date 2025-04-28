70c5edaf0ebfceb04a71694eb08b227d625e8831
great-luao <luao@shanghaitech.edu.cn>
2025-04-28 14:37:31 +0800

    add some components in bon eval

--- bon_eval_hf.py
+++ bon_eval_hf.py
@@ -66,6 +66,13 @@ def best_of_n(splitted_completions):
         selected_completions.append(n_completions_per_query[0])
     return selected_completions
 
+def best_of_n_last(splitted_completions):
+    selected_completions = []
+    for n_completions_per_query in splitted_completions:
+        n_completions_per_query = sorted(n_completions_per_query, key=lambda x: x["step_reward"][-1], reverse=True)
+        assert all([n_completions_per_query[0]["step_reward"][-1] >= completion["step_reward"][-1] for completion in n_completions_per_query])
+        selected_completions.append(n_completions_per_query[0])
+    return selected_completions
 
 def compute_metrics(dataset_name, scored_results):
     metrics = {}
@@ -83,7 +90,7 @@ def compute_metrics(dataset_name, scored_results):
         splitted_completions = split_query(scored_results, n, max(sample_nums))
         if not args.baseline and not args.combine:
 
-            selected_completions = best_of_n(splitted_completions)
+            selected_completions = best_of_n_last(splitted_completions)
             print("Length of selected_completions: ", len(selected_completions))
             print("Length of original_dataset: ", len(original_dataset))
             assert len(original_dataset) == len(selected_completions)
@@ -145,175 +152,181 @@ if __name__=='__main__':
     parser.add_argument("--combine", type=int, default=0)
     parser.add_argument("--orm", type=int, default=0)
     parser.add_argument("--backbone-path", type=str, default="/storage/group/renkan/luao/pretrain/deepseek-math-7b-base")
-    parser.add_argument("--model-path", type=str, default="/public/home/luao/LLM/Process_Q_Model/nobackup/prm_checkpoints/neg-zeta-16/checkpoint-1043/pytorch_model.bin")
+    parser.add_argument("--model-path", type=str, default="/public/home/luao/LLM/Process_Q_Model/nobackup/prm_checkpoints/con-loss/checkpoint-531/pytorch_model.bin")
     # parser.add_argument("--model-path", type=str, default="/storage/group/renkan/luao/pretrain/PQM/zeta-4/model.safetensors")
     parser.add_argument("--data-name", type=str,choices=['math','gsm8k'])
     parser.add_argument("--data-file", type=str,required=True)
-    parser.add_argument("--save-file", type=str,default="./bon_result/prm-data-selftest.json")
+    parser.add_argument("--save-file", type=str,default="./bon_result/con-prm-data-selftest.json")
 
     args = parser.parse_args()
     print(args)
 
-
     seed_everything(0)
     accelerator = Accelerator()
-    if not args.baseline:
-        prm_token = '[PRM]'
-        model_path = args.backbone_path
-        tokenizer = AutoTokenizer.from_pretrained(model_path)
-        if not tokenizer.pad_token:
-            tokenizer.pad_token = tokenizer.eos_token
-        model = AutoModelForCausalLM.from_pretrained(model_path,
-                                                     torch_dtype=torch.bfloat16)
-        tokenizer.add_special_tokens({'additional_special_tokens':[prm_token]})
-        prm_token_id = tokenizer.encode(prm_token, add_special_tokens=False)[-1]
-        model.resize_token_embeddings(len(tokenizer))
-        model = AutoModelForCausalLMWithValueHead(model)
-        
-        print('loading model weights from', args.model_path)
-        if '.safetensor' in args.model_path:
-            state_dict = {}
-            with safe_open(args.model_path, framework="pt", device="cpu") as f:
-                for key in f.keys():
-                    state_dict[key] = f.get_tensor(key)
-        else:
-            state_dict = torch.load(args.model_path, weights_only=True)
-        model.load_state_dict(state_dict)
-
-        print("Init deepspeed engine")
-        ds_engine = deepspeed.init_inference(model,
-                                             tensor_parallel={"tp_size": 1},
-                                             dtype=torch.bfloat16)
-
-        model = ds_engine.module
-        model.eval()
-
-        def data_collator(example, tokenizer=tokenizer):
-            inputs = []
-            special_ids = []
-            step_labels = []
-            orm_ids = []
-            idx,reward_idx = [],[]
-            template = '{query}\n{answer}'
-            for d in example:
-                input_ids = tokenizer.encode(template.format(query=d['query'],answer=d['answer']),
-                                              add_special_tokens=False)
-                inputs.append(torch.tensor(input_ids))
-
-                cur_special_ids = []
-                for ii,id in enumerate(input_ids):
-                    if id==prm_token_id:
-                        cur_special_ids.append(ii)
-                # assert len(cur_special_ids)==len(d['labels'])
-                special_ids.append(torch.tensor(cur_special_ids))
-                orm_ids.append(cur_special_ids[-1])
-                # step_labels.append(torch.tensor(d['labels']))
-                idx.append(d['idx'])
-                reward_idx.append(d['reward_idx'])
-
-            inputs = pad_sequence(inputs, padding_value=tokenizer.pad_token_id, batch_first=True)
-            attention_mask = (inputs!=tokenizer.pad_token_id)
-            special_ids = pad_sequence(special_ids, padding_value=-100, batch_first=True)
-            # step_labels = pad_sequence(step_labels, padding_value=-100, batch_first=True)
-
-            return {
-                'input_ids': inputs.int().to(accelerator.device),
-                'attention_mask': attention_mask.int().to(accelerator.device),
-                'special_tokens':special_ids.to(accelerator.device),
-                'orm_tokens': torch.tensor(orm_ids).to(accelerator.device),
-                'idx':torch.tensor(idx).to(accelerator.device),
-                'reward_idx':torch.tensor(reward_idx).to(accelerator.device)
-            }
     data_name = args.data_name
 
-    if data_name == 'gsm8k':
-        file_list = [
-            args.data_file,
-        ]
-        queries = []
-        cur_queries = []
-        origin_dataset = load_dataset('qintongli/GSM-Plus')['testmini']
-        for file_name in file_list:
-            cur_data = json.load(open(file_name))
-            if len(cur_queries) == len(cur_data):
-                for cur_q, cur_d in zip(cur_queries, cur_data):
-                    cur_q['responses'].extend(cur_d['responses'])
-            else:
-                cur_queries = deepcopy(cur_data)
+    # if not args.baseline:
+    #     prm_token = '[PRM]'
+    #     model_path = args.backbone_path
+    #     tokenizer = AutoTokenizer.from_pretrained(model_path)
+    #     if not tokenizer.pad_token:
+    #         tokenizer.pad_token = tokenizer.eos_token
+    #     model = AutoModelForCausalLM.from_pretrained(model_path,
+    #                                                  torch_dtype=torch.bfloat16)
+    #     tokenizer.add_special_tokens({'additional_special_tokens':[prm_token]})
+    #     prm_token_id = tokenizer.encode(prm_token, add_special_tokens=False)[-1]
+    #     model.resize_token_embeddings(len(tokenizer))
+    #     model = AutoModelForCausalLMWithValueHead(model)
+        
+    #     print('loading model weights from', args.model_path)
+    #     if '.safetensor' in args.model_path:
+    #         state_dict = {}
+    #         with safe_open(args.model_path, framework="pt", device="cpu") as f:
+    #             for key in f.keys():
+    #                 state_dict[key] = f.get_tensor(key)
+    #     else:
+    #         state_dict = torch.load(args.model_path, weights_only=True)
+    #     model.load_state_dict(state_dict)
 
-        assert len(origin_dataset) == len(cur_queries), (len(origin_dataset), len(queries))
-        for idx, (data, ori) in enumerate(zip(cur_queries, origin_dataset)):
-            assert data['question'] == ori['question']
-            assert len(data['responses']) == 128
-            for response_dict in data['responses']:
-                queries.append({
-                    'idx': idx,
-                    'prompt': data['question'],
-                    'response': response_dict['text'],
-                    'solution': ori['answer'],
-                    'logprobs': 0,
-                })
-    elif data_name == 'math':
-        file_list = [
-            args.data_file,
-        ]
-        queries = []
-        cur_queries = []
-        path = '/storage/group/renkan/luao/original_datasets/mathQA-datasets/math500/test.jsonl'
-        with open(path) as f:
-            origin_dataset = [json.loads(line) for line in f]
-        for file_name in file_list:
-            cur_data = json.load(open(file_name))
-            if len(cur_queries) == len(cur_data):
-                for cur_q, cur_d in zip(cur_queries, cur_data):
-                    cur_q['responses'].extend(cur_d['responses'])
-            else:
-                cur_queries = deepcopy(cur_data)
+    #     print("Init deepspeed engine")
+    #     deepspeed_config = json.load(open('accelerate_configs/deepspeed_3.json'))
+    #     ds_engine = deepspeed.init_inference(model,
+    #                                          tensor_parallel={"tp_size": 1},
+    #                                          dtype=torch.bfloat16)
 
-        assert len(origin_dataset) == len(cur_queries), (len(origin_dataset), len(queries))
-        for idx, (data, ori) in enumerate(zip(cur_queries, origin_dataset)):
-            assert data['question'] == ori['problem']
-            # assert len(data['responses']) % 128==0
-            for response_dict in data['responses']:
-                queries.append({
-                    'idx': idx,
-                    'prompt': data['question'],
-                    'response': response_dict['text'],
-                    'solution': ori['solution'],
-                    'logprobs': 0,
-                })
+    #     model = ds_engine.module
+    #     model.eval()
 
-    if not args.baseline:
-        for idx, data in enumerate(queries):
-            data['reward_idx'] = idx
-            data["query"] = instruction_format(data["prompt"])
-            steps = re.split('Step \d+:', data['response'])
-            steps = [f'Step {id + 1}: ' + step.strip() for id, step in enumerate(steps) if step.strip()!='']
-            data["answer"] = f" {prm_token}\n".join(steps) + f" {prm_token}"
-            if(idx <= 1):
-                print(data['answer'])
+    #     def data_collator(example, tokenizer=tokenizer):
+    #         inputs = []
+    #         special_ids = []
+    #         step_labels = []
+    #         orm_ids = []
+    #         idx,reward_idx = [],[]
+    #         template = '{query}\n{answer}'
+    #         for d in example:
+    #             input_ids = tokenizer.encode(template.format(query=d['query'],answer=d['answer']),
+    #                                           add_special_tokens=False)
+    #             inputs.append(torch.tensor(input_ids))
+
+    #             cur_special_ids = []
+    #             for ii,id in enumerate(input_ids):
+    #                 if id==prm_token_id:
+    #                     cur_special_ids.append(ii)
+    #             # assert len(cur_special_ids)==len(d['labels'])
+    #             special_ids.append(torch.tensor(cur_special_ids))
+    #             orm_ids.append(cur_special_ids[-1])
+    #             # step_labels.append(torch.tensor(d['labels']))
+    #             idx.append(d['idx'])
+    #             reward_idx.append(d['reward_idx'])
+
+    #         inputs = pad_sequence(inputs, padding_value=tokenizer.pad_token_id, batch_first=True)
+    #         attention_mask = (inputs!=tokenizer.pad_token_id)
+    #         special_ids = pad_sequence(special_ids, padding_value=-100, batch_first=True)
+    #         # step_labels = pad_sequence(step_labels, padding_value=-100, batch_first=True)
+
+    #         return {
+    #             'input_ids': inputs.int().to(accelerator.device),
+    #             'attention_mask': attention_mask.int().to(accelerator.device),
+    #             'special_tokens':special_ids.to(accelerator.device),
+    #             'orm_tokens': torch.tensor(orm_ids).to(accelerator.device),
+    #             'idx':torch.tensor(idx).to(accelerator.device),
+    #             'reward_idx':torch.tensor(reward_idx).to(accelerator.device)
+    #         }
+
+    # if data_name == 'gsm8k':
+    #     file_list = [
+    #         args.data_file,
+    #     ]
+    #     queries = []
+    #     cur_queries = []
+    #     origin_dataset = load_dataset('qintongli/GSM-Plus')['testmini']
+    #     for file_name in file_list:
+    #         cur_data = json.load(open(file_name))
+    #         if len(cur_queries) == len(cur_data):
+    #             for cur_q, cur_d in zip(cur_queries, cur_data):
+    #                 cur_q['responses'].extend(cur_d['responses'])
+    #         else:
+    #             cur_queries = deepcopy(cur_data)
+
+    #     assert len(origin_dataset) == len(cur_queries), (len(origin_dataset), len(queries))
+    #     for idx, (data, ori) in enumerate(zip(cur_queries, origin_dataset)):
+    #         assert data['question'] == ori['question']
+    #         assert len(data['responses']) == 128
+    #         for response_dict in data['responses']:
+    #             queries.append({
+    #                 'idx': idx,
+    #                 'prompt': data['question'],
+    #                 'response': response_dict['text'],
+    #                 'solution': ori['answer'],
+    #                 'logprobs': 0,
+    #             })
+    # elif data_name == 'math':
+    #     file_list = [
+    #         args.data_file,
+    #     ]
+    #     queries = []
+    #     cur_queries = []
+    #     path = '/storage/group/renkan/luao/original_datasets/mathQA-datasets/math500/test.jsonl'
+    #     with open(path) as f:
+    #         origin_dataset = [json.loads(line) for line in f]
+    #     for file_name in file_list:
+    #         cur_data = json.load(open(file_name))
+    #         if len(cur_queries) == len(cur_data):
+    #             for cur_q, cur_d in zip(cur_queries, cur_data):
+    #                 cur_q['responses'].extend(cur_d['responses'])
+    #         else:
+    #             cur_queries = deepcopy(cur_data)
+
+    #     assert len(origin_dataset) == len(cur_queries), (len(origin_dataset), len(queries))
+    #     for idx, (data, ori) in enumerate(zip(cur_queries, origin_dataset)):
+    #         assert data['question'] == ori['problem']
+    #         # assert len(data['responses']) % 128==0
+    #         for response_dict in data['responses']:
+    #             queries.append({
+    #                 'idx': idx,
+    #                 'prompt': data['question'],
+    #                 'response': response_dict['text'],
+    #                 'solution': ori['solution'],
+    #                 'logprobs': 0,
+    #             })
+
+    # if not args.baseline:
+    #     for idx, data in enumerate(queries):
+    #         data['reward_idx'] = idx
+    #         data["query"] = instruction_format(data["prompt"])
+    #         steps = re.split('Step \d+:', data['response'])
+    #         steps = [f'Step {id + 1}: ' + step.strip() for id, step in enumerate(steps) if step.strip()!='']
+    #         data["answer"] = f" {prm_token}\n".join(steps) + f" {prm_token}"
+    #         if(idx <= 1):
+    #             print(data['answer'])
 
 
-        dataset = Dataset.from_pandas(pd.DataFrame.from_records(queries))
-        dataloader = DataLoader(dataset,batch_size=4,shuffle=False,collate_fn=data_collator)
+    #     dataset = Dataset.from_pandas(pd.DataFrame.from_records(queries))
+    #     dataloader = DataLoader(dataset,batch_size=4,shuffle=False,collate_fn=data_collator)
 
 
-        for inputs in tqdm(dataloader):
-            with torch.no_grad():
-                _, _, rewards = model(input_ids=inputs['input_ids'], attention_mask=inputs['attention_mask'])
-            cur_index = torch.where(inputs['special_tokens']==-100,0,inputs['special_tokens'])
-            if not args.orm:
-                rewards = rewards.gather(dim=-1, index=cur_index)
-                final_rewards = torch.where(inputs['special_tokens']==-100,1e5,rewards).min(-1).values
-            else:
-                rewards = rewards.gather(dim=-1, index=inputs['orm_tokens'][...,None])
-                final_rewards = rewards.squeeze(1)
-            for step_reward,final_reward,reward_idx in zip(rewards.tolist(),final_rewards.tolist(),inputs['reward_idx'].tolist()):
-                queries[int(reward_idx)]['reward'] = final_reward
-                queries[int(reward_idx)]['step_reward'] = [r for r in step_reward if r!=1e5]
+    #     for i, inputs in enumerate(tqdm(dataloader)):
+    #         with torch.no_grad():
+    #             _, _, rewards = model(input_ids=inputs['input_ids'], attention_mask=inputs['attention_mask'])
+    #         cur_index = torch.where(inputs['special_tokens']==-100,0,inputs['special_tokens'])
+    #         if not args.orm:
+    #             rewards = rewards.gather(dim=-1, index=cur_index)
+    #             final_rewards = torch.where(inputs['special_tokens']==-100,1e5,rewards).min(-1).values
+    #         else:
+    #             rewards = rewards.gather(dim=-1, index=inputs['orm_tokens'][...,None])
+    #             final_rewards = rewards.squeeze(1)
+    #         for step_reward,final_reward,reward_idx in zip(rewards.tolist(),final_rewards.tolist(),inputs['reward_idx'].tolist()):
+    #             queries[int(reward_idx)]['reward'] = final_reward
+    #             queries[int(reward_idx)]['step_reward'] = [r for r in step_reward if r!=1e5]
+    #         # Free up the memory for rewards
+    #         if i <= 0:
+    #             # Check the device of the rewards
+    #             print(rewards.device,final_rewards.device)
+    #         del rewards, final_rewards
 
-    with open(args.save_file,'w') as f:
-        json.dump(queries,f)
+    # with open(args.save_file,'w') as f:
+    #     json.dump(queries,f)
     queries = json.load(open(args.save_file))
     compute_metrics(data_name, queries)
 
--- scripts/bon_eval.sh
+++ scripts/bon_eval.sh
@@ -1,1 +1,2 @@
-CUDA_VISIBLE_DEVICES=0 deepspeed bon_eval_hf.py --data-name 'math' --data-file "bon_test_set/math500.json"
+export CUDA_VISIBLE_DEVICES=0 
+deepspeed --num_gpus=1 bon_eval_hf.py --data-name 'math' --data-file "bon_test_set/math500.json" --orm 1
